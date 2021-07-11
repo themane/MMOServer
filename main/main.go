6c3766f5a86176d840261efdff5b95372a27fa3e
Devashish <devashish.gupta@target.com>
2021-07-12 00:19:22 +0530

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", ping)
+	err := r.Run()
+	if err != nil {
+		log.Println("Error in starting server")
+		return
+	}
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
