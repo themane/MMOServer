95ffdfbac3b59767c2a010dfd2c8422cf77cd1f5
Devashish <devashish.gupta@target.com>
2021-09-02 19:47:15 +0530

    Adding support for basic login API

---
+++ LoginRequest.go
@@ -0,0 +1,67 @@
+package main
+
+type LoginRequest struct {
+	Username string `json:"username"`
+}
+
+type LoginResponse struct {
+	PlanetConfig string     `json:"planet_config"`
+	Position     Position   `json:"position"`
+	Water        Resource   `json:"water"`
+	Graphene     Resource   `json:"graphene"`
+	Shelio       Resource   `json:"shelio"`
+	Population   Population `json:"population"`
+	Mines        []Mine     `json:"mines"`
+}
+
+type Position struct {
+	System  int    `json:"system"`
+	Sector  int    `json:"sector"`
+	Planet  int    `json:"planet"`
+	Display string `json:"display"`
+}
+
+type Resource struct {
+	GenerationRate int `json:"generation_rate"`
+	Amount         int `json:"amount"`
+	Reserved       int `json:"reserved"`
+}
+
+type Population struct {
+	Total          int                `json:"total"`
+	GenerationRate int                `json:"generation_rate"`
+	Unemployed     int                `json:"unemployed"`
+	Workers        EmployedPopulation `json:"workers"`
+	Soldiers       EmployedPopulation `json:"soldiers"`
+}
+
+type EmployedPopulation struct {
+	Total int `json:"total"`
+	Idle  int `json:"idle"`
+}
+
+type Mine struct {
+	Id          string      `json:"_id"`
+	Type        string      `json:"type"`
+	MaxLimit    int         `json:"max_limit"`
+	Mined       int         `json:"mined"`
+	MiningPlant MiningPlant `json:"mining_plant"`
+}
+
+type MiningPlant struct {
+	BuildingId          string              `json:"building_id"`
+	BuildingLevel       int                 `json:"building_level"`
+	MiningRate          int                 `json:"mining_rate"`
+	Workers             int                 `json:"workers"`
+	NextLevelAttributes NextLevelAttributes `json:"next_level"`
+}
+
+type NextLevelAttributes struct {
+	GrapheneRequired           int `json:"graphene_required"`
+	WaterRequired              int `json:"water_required"`
+	ShelioRequired             int `json:"shelio_required"`
+	CurrentMiningRatePerWorker int `json:"current_mining_rate_per_worker"`
+	NextMiningRatePerWorker    int `json:"next_mining_rate_per_worker"`
+	CurrentWorkersMaxLimit     int `json:"current_workers_max_limit"`
+	NextWorkersMaxLimit        int `json:"next_workers_max_limit"`
+}
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", ping)
+	r.POST("/login", login)
 	err := r.Run()
 	if err != nil {
 		log.Println("Error in starting server")
@@ -19,4 +23,33 @@ func ping(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Test 3 Pong",
 	})
+}
+
+func login(c *gin.Context) {
+	body, _ := ioutil.ReadAll(c.Request.Body)
+	var request LoginRequest
+	json.Unmarshal(body, &request)
+	log.Printf("Logged in user: %s", request.Username)
+
+	var response LoginResponse
+	switch request.Username {
+	case "devashish":
+		jsonFile, _ := os.Open("sample_responses/PlanetConfigResponse1.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &response)
+	case "nehal":
+		jsonFile, _ := os.Open("sample_responses/PlanetConfigResponse2.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &response)
+	case "parth":
+		jsonFile, _ := os.Open("sample_responses/PlanetConfigResponse3.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &response)
+	case "sneha":
+		jsonFile, _ := os.Open("sample_responses/PlanetConfigResponse4.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &response)
+	}
+	c.JSON(200, response)
+	//c.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
 }
