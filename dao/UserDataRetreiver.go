788cb75f0980351445601b7055c8be3c047bcf28
Devashish <devashish.gupta@target.com>
2021-09-12 06:16:12 +0530

    Adding support for dao

---
+++ dao/UserDataRetreiver.go
@@ -0,0 +1,36 @@
+package dao
+
+import (
+	"encoding/json"
+	"github.com/themane/MMOServer/models"
+	"io/ioutil"
+	"os"
+)
+
+func GetUserData(Username string) models.UserData {
+
+	var userData models.UserData
+	switch Username {
+	case "devashish":
+		jsonFile, _ := os.Open("sample_user_data/devashish.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &userData)
+	case "nehal":
+		jsonFile, _ := os.Open("sample_user_data/nehal.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &userData)
+	case "parth":
+		jsonFile, _ := os.Open("sample_user_data/parth.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &userData)
+	case "sneha":
+		jsonFile, _ := os.Open("sample_user_data/sneha.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &userData)
+	case "sweta":
+		jsonFile, _ := os.Open("sample_user_data/sweta.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(responseByteValue, &userData)
+	}
+	return userData
+}
