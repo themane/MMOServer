51c44e73a31d8263eae8e10c260c7585fa34a124
Devashish <devashish.gupta@target.com>
2021-09-12 13:56:19 +0530

    Updating model for Universe

--- models/Universe.go
+++ models/Universe.go
@@ -1,14 +1,31 @@
 package models
 
 type Universe struct {
-	Planets map[string]struct {
-		Mines []struct {
-			Id           string `json:"_id"`
-			Type         string `json:"type"`
-			MaxLimit     int    `json:"max_limit"`
-			IncreaseRate int    `json:"increase_rate"`
-		} `json:"mines"`
-		Position     PlanetPosition `json:"position"`
-		PlanetConfig string         `json:"planet_config"`
-	} `json:"planets"`
+	NumSystems int       `json:"num_systems"`
+	Systems    []SystemU `json:"systems"`
+}
+
+type SystemU struct {
+	System     int       `json:"system"`
+	NumSystems int       `json:"num_systems"`
+	Sectors    []SectorU `json:"sectors"`
+}
+
+type SectorU struct {
+	Sector     int       `json:"sector"`
+	NumPlanets int       `json:"num_planets"`
+	Planets    []PlanetU `json:"planets"`
+}
+
+type PlanetU struct {
+	Planet       int     `json:"planet"`
+	Mines        []MineU `json:"mines"`
+	PlanetConfig string  `json:"planet_config"`
+}
+
+type MineU struct {
+	Id           string `json:"_id"`
+	Type         string `json:"type"`
+	MaxLimit     int    `json:"max_limit"`
+	IncreaseRate int    `json:"increase_rate"`
 }
