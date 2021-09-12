1dbb6c8943d70c5b9eaa37dde5d50e9938d3875e
Devashish <devashish.gupta@target.com>
2021-09-12 18:30:15 +0530

    Reformating

--- models/Universe.go
+++ models/Universe.go
@@ -1,29 +1,29 @@
 package models
 
 type Universe struct {
-	NumSystems int                `json:"num_systems"`
-	Systems    map[string]SystemU `json:"systems"`
+	NumSystems int                  `json:"num_systems"`
+	Systems    map[string]SystemUni `json:"systems"`
 }
 
-type SystemU struct {
-	System     int                `json:"system"`
-	NumSystems int                `json:"num_systems"`
-	Sectors    map[string]SectorU `json:"sectors"`
+type SystemUni struct {
+	System     int                  `json:"system"`
+	NumSystems int                  `json:"num_systems"`
+	Sectors    map[string]SectorUni `json:"sectors"`
 }
 
-type SectorU struct {
-	Sector     int                `json:"sector"`
-	NumPlanets int                `json:"num_planets"`
-	Planets    map[string]PlanetU `json:"planets"`
+type SectorUni struct {
+	Sector     int                  `json:"sector"`
+	NumPlanets int                  `json:"num_planets"`
+	Planets    map[string]PlanetUni `json:"planets"`
 }
 
-type PlanetU struct {
-	Planet       int     `json:"planet"`
-	Mines        []MineU `json:"mines"`
-	PlanetConfig string  `json:"planet_config"`
+type PlanetUni struct {
+	Planet       int       `json:"planet"`
+	Mines        []MineUni `json:"mines"`
+	PlanetConfig string    `json:"planet_config"`
 }
 
-type MineU struct {
+type MineUni struct {
 	Id           string `json:"_id"`
 	Type         string `json:"type"`
 	MaxLimit     int    `json:"max_limit"`
--- models/UserData.go
+++ models/UserData.go
@@ -1,6 +1,42 @@
 package models
 
 type UserData struct {
-	Profile         Profile          `json:"profile"`
-	OccupiedPlanets []OccupiedPlanet `json:"occupied_planets"`
+	Profile         Profile      `json:"profile"`
+	OccupiedPlanets []PlanetUser `json:"occupied_planets"`
+}
+
+type PlanetUser struct {
+	PlanetConfig string         `json:"planet_config" example:"Planet2.json"`
+	Position     PlanetPosition `json:"position"`
+	Water        ResourceUser   `json:"water"`
+	Graphene     ResourceUser   `json:"graphene"`
+	Shelio       int            `json:"shelio" example:"23"`
+	Population   PopulationUser `json:"population"`
+	Mines        []MineUser     `json:"mines"`
+	Home         bool           `json:"home" example:"true"`
+}
+
+type ResourceUser struct {
+	Amount   int `json:"amount" example:"23"`
+	Reserved int `json:"reserved" example:"14"`
+}
+
+type PopulationUser struct {
+	GenerationRate int                `json:"generation_rate" example:"3"`
+	Unemployed     int                `json:"unemployed" example:"3"`
+	Workers        EmployedPopulation `json:"workers"`
+	Soldiers       EmployedPopulation `json:"soldiers"`
+}
+
+type MineUser struct {
+	Id          string      `json:"_id" example:"W101"`
+	Type        string      `json:"type" example:"WATER"`
+	Mined       int         `json:"mined" example:"125"`
+	MiningPlant MiningPlant `json:"mining_plant"`
+}
+
+type MiningPlantUser struct {
+	BuildingId    string `json:"building_id" example:"WMP101"`
+	BuildingLevel int    `json:"building_level" example:"3"`
+	Workers       int    `json:"workers" example:"12"`
 }
