78402d5db0fb7a19302292c9dbf8cd4d1fabc883
Devashish <devashish.gupta@target.com>
2021-09-14 02:32:45 +0530

    Updating clan data retreival

--- dao/ClanDataRetreiver.go
+++ dao/ClanDataRetreiver.go
@@ -8,10 +8,25 @@ import (
 	"os"
 )
 
-func GetClanData(Name string) models.ClanData {
+func GetNamedClan(Name string) models.ClanData {
 	var clanData models.ClanData
 	switch Name {
 	case "MindKrackers":
+		jsonFile, _ := os.Open("sample_user_data/mind_krackers.json")
+		responseByteValue, _ := ioutil.ReadAll(jsonFile)
+		err := json.Unmarshal(responseByteValue, &clanData)
+		if err != nil {
+			log.Print(err)
+			return models.ClanData{}
+		}
+	}
+	return clanData
+}
+
+func GetClan(Id string) models.ClanData {
+	var clanData models.ClanData
+	switch Id {
+	case "6f744cf6-c873-4c80-8fa6-62da47d87090":
 		jsonFile, _ := os.Open("sample_user_data/mind_krackers.json")
 		responseByteValue, _ := ioutil.ReadAll(jsonFile)
 		err := json.Unmarshal(responseByteValue, &clanData)
