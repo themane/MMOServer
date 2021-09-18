53e558fd744cdd34c192de2267f7b5055cb76633
Devashish <devashish.gupta@target.com>
2021-09-19 05:09:41 +0530

    Adding update methods for repository

--- mongoRepository/UserRepository.go
+++ mongoRepository/UserRepository.go
@@ -5,6 +5,8 @@ import (
 	"github.com/themane/MMOServer/mongoRepository/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"math"
 )
 
 type UserRepositoryImpl struct {
@@ -47,4 +49,114 @@ func (u *UserRepositoryImpl) FindByUsername(username string) (*models.UserData, 
 		return nil, err
 	}
 	return result, nil
+}
+
+func (u *UserRepositoryImpl) AddExperience(username string, experience int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"profile.experience", experience}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Added experience username: %s, experience: %d\n", username, experience)
+	return nil
+}
+
+func (u *UserRepositoryImpl) UpdateClanId(username string, clanId string) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{{"profile.clan_id", clanId}}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Updated username: %s, clanId: %s\n", username, clanId)
+	return nil
+}
+
+func (u *UserRepositoryImpl) UpgradeBuildingLevel(username string, planetId string, buildingId string,
+	waterRequired int, grapheneRequired int, shelioRequired int) error {
+
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".buildings." + buildingId, 1}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".water.amount", -waterRequired}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".graphene.", -grapheneRequired}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".shelio", -shelioRequired}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Upgraded username: %s, planetId: %s, buildingId: %s\n", username, planetId, buildingId)
+	return nil
+}
+
+func (u *UserRepositoryImpl) AddResources(username string, planetId string, water int, graphene int, shelio int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".water.amount", water}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".graphene.amount", graphene}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".shelio", shelio}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Added username: %s, planetId: %s, water: %d, graphene: %d, shelio: %d\n", username, planetId, water, graphene, shelio)
+	return nil
+}
+
+func (u *UserRepositoryImpl) UpdateMineResources(username string, planetId string, mineId string, water int, graphene int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".water.amount", water}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".water.amount", water}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".graphene.amount", graphene}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".mines." + mineId + "mined", math.Max(float64(water), float64(graphene))}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Updated mine resources username: %s, planetId: %s, water: %d, graphene: %d, mineId: %s\n",
+		username, planetId, water, graphene, mineId)
+	return nil
+}
+
+func (u *UserRepositoryImpl) UpdateWorkers(username string, planetId string, buildingId string, workers int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$set", bson.D{{"occupied_planets." + planetId + ".buildings." + buildingId + "workers", workers}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Updated workers username: %s, planetId: %s, buildingId: %s, workers: %d\n", username, planetId, buildingId, workers)
+	return nil
+}
+
+func (u *UserRepositoryImpl) AddPopulation(username string, planetId string, population int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".population.unemployed", population}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Added population username: %s, planetId: %s, population: %d\n", username, planetId, population)
+	return nil
+}
+
+func (u *UserRepositoryImpl) RecruitWorkers(username string, planetId string, workers int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".population.unemployed", -workers}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".population.workers.idle", workers}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Assigned workers username: %s, planetId: %s, workers: %d\n", username, planetId, workers)
+	return nil
+}
+
+func (u *UserRepositoryImpl) RecruitSoldiers(username string, planetId string, soldiers int) error {
+	defer disconnect(u.client, u.ctx)
+	filter := bson.D{{"profile.username", username}}
+	update := bson.D{
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".population.unemployed", -soldiers}}},
+		{"$inc", bson.D{{"occupied_planets." + planetId + ".population.soldiers.idle", soldiers}}},
+	}
+	u.getCollection().FindOneAndUpdate(u.ctx, filter, update)
+	log.Printf("Assigned soldiers username: %s, planetId: %s, soldiers: %d\n", username, planetId, soldiers)
+	return nil
 }
--- mongoRepository/models/UserData.go
+++ mongoRepository/models/UserData.go
@@ -15,12 +15,13 @@ type ProfileUser struct {
 }
 
 type PlanetUser struct {
-	Water      ResourceUser        `json:"water"`
-	Graphene   ResourceUser        `json:"graphene"`
-	Shelio     int                 `json:"shelio" example:"23"`
-	Population PopulationUser      `json:"population"`
-	Mines      map[string]MineUser `json:"mines"`
-	Home       bool                `json:"home" example:"true"`
+	Water      ResourceUser            `json:"water"`
+	Graphene   ResourceUser            `json:"graphene"`
+	Shelio     int                     `json:"shelio" example:"23"`
+	Population PopulationUser          `json:"population"`
+	Mines      map[string]MineUser     `json:"mines"`
+	Buildings  map[string]BuildingUser `json:"buildings"`
+	Home       bool                    `json:"home" example:"true"`
 }
 
 type ResourceUser struct {
@@ -36,15 +37,27 @@ type PopulationUser struct {
 }
 
 type MineUser struct {
-	Mined       int `json:"mined" example:"125"`
-	MiningPlant struct {
-		Id            string `json:"building_id" example:"WMP101"`
-		BuildingLevel int    `json:"building_level" example:"3"`
-		Workers       int    `json:"workers" example:"12"`
-	} `json:"mining_plant"`
+	Mined         int    `json:"mined" example:"125"`
+	MiningPlantId string `json:"mining_plant_id"`
+}
+
+type BuildingUser struct {
+	BuildingLevel int `json:"building_level" example:"3"`
+	Workers       int `json:"workers" example:"12"`
 }
 
 type UserRepository interface {
 	FindById(id string) (*UserData, error)
 	FindByUsername(username string) (*UserData, error)
+
+	AddExperience(username string, experience int) error
+	UpdateClanId(username string, clanId string) error
+
+	UpgradeBuildingLevel(username string, planetId string, buildingId string, waterRequired int, grapheneRequired int, shelioRequired int) error
+	AddResources(username string, planetId string, water int, graphene int, shelio int) error
+	UpdateMineResources(username string, planetId string, water int, graphene int) error
+	UpdateWorkers(username string, planetId string, buildingId string, workers int) error
+	AddPopulation(username string, planetId string, population int) error
+	RecruitWorkers(username string, planetId string, worker int) error
+	RecruitSoldiers(username string, planetId string, soldiers int) error
 }
