2c0483e957adb617cfeea43b6bb01b403d5b5220
Devashish <devashish.gupta@target.com>
2021-09-19 03:48:02 +0530

    Adding support for LoginService

--- services/LoginService.go
+++ services/LoginService.go
@@ -1,68 +1,100 @@
 package services
 
 import (
-	"github.com/themane/MMOServer/dao"
+	"github.com/themane/MMOServer/constants"
+	controllerModels "github.com/themane/MMOServer/controllers/models"
 	"github.com/themane/MMOServer/models"
+	repoModels "github.com/themane/MMOServer/mongoRepository/models"
 )
 
-func Login(username string) models.LoginResponse {
-	universe := dao.GetUniverse()
-	waterConstants := dao.GetWaterConstants()
-	grapheneConstants := dao.GetGrapheneConstants()
-	experienceConstants := dao.GetExperienceConstants()
-	userData := dao.GetUserData(username)
-	clanData := dao.GetClan(userData.Profile.ClanId)
-	homePlanetPosition := findHomePlanet(userData.OccupiedPlanets)
+func Login(username string, userRepository repoModels.UserRepository, clanRepository repoModels.ClanRepository, universeRepository repoModels.UniverseRepository) (*controllerModels.LoginResponse, error) {
+	waterConstants := constants.GetWaterConstants()
+	grapheneConstants := constants.GetGrapheneConstants()
+	experienceConstants := constants.GetExperienceConstants()
 
-	var response models.LoginResponse
-	response.Profile.Init(userData.Profile, clanData, experienceConstants)
-	response.HomeSector, response.HomePlanet = home(userData.OccupiedPlanets, *homePlanetPosition, universe, waterConstants, grapheneConstants)
-	response.OccupiedPlanets = occupiedPlanets(userData.OccupiedPlanets, homePlanetPosition.SectorId(), universe)
+	userData, err := userRepository.FindByUsername(username)
+	if err != nil {
+		return nil, err
+	}
+	clanData, err := getClanData(userData.Profile.ClanId, clanRepository)
+	if err != nil {
+		return nil, err
+	}
+	homePlanetPosition, homeSectorData, err := getHomeSectorData(userData, universeRepository)
+	if err != nil {
+		return nil, err
+	}
 
-	return response
+	var response controllerModels.LoginResponse
+	response.Profile.Init(*userData, clanData, experienceConstants)
+	response.HomeSector, response.HomePlanetId = home(userData.OccupiedPlanets, *homePlanetPosition, homeSectorData, waterConstants, grapheneConstants)
+	response.OccupiedPlanets = occupiedPlanets(userData.OccupiedPlanets, homePlanetPosition.SectorId(), homeSectorData)
+	return &response, nil
 }
 
-func home(allOccupiedPlanetIds map[string]models.PlanetUser, homePlanetPosition models.PlanetPosition, universe models.Universe,
-	waterConstants models.ResourceConstants, grapheneConstants models.ResourceConstants) (models.Sector, models.OccupiedPlanet) {
-	sectorU := universe.Systems[homePlanetPosition.SystemId()].Sectors[homePlanetPosition.SectorId()]
-	var homeSector models.Sector
-	var homePlanet models.OccupiedPlanet
+func home(allOccupiedPlanetIds map[string]repoModels.PlanetUser,
+	homePlanetPosition models.PlanetPosition,
+	homeSectorData map[string]repoModels.PlanetUni,
+	waterConstants constants.ResourceConstants,
+	grapheneConstants constants.ResourceConstants) (controllerModels.Sector, string) {
+
+	var homeSector controllerModels.Sector
+	var homePlanetId string
 
 	homeSector.Position.Init(homePlanetPosition)
 
-	for key, planetUni := range sectorU.Planets {
-		if planetUser, ok := allOccupiedPlanetIds[key]; ok {
-			planetData := models.OccupiedPlanet{}
-			planetData.Init(planetUni, planetUser, homeSector.Position, waterConstants, grapheneConstants)
+	for planetId, planetUni := range homeSectorData {
+		if planetUser, ok := allOccupiedPlanetIds[planetId]; ok {
+			planetData := controllerModels.OccupiedPlanet{}
+			planetData.Init(planetUni, planetUser, waterConstants, grapheneConstants)
 			if planetData.Home {
-				homePlanet = planetData
+				homePlanetId = planetId
 			}
 			homeSector.OccupiedPlanets = append(homeSector.OccupiedPlanets, planetData)
 			continue
 		}
-		planetData := models.UnoccupiedPlanet{}
-		planetData.Init(planetUni, homeSector.Position)
+		planetData := controllerModels.UnoccupiedPlanet{}
+		planetData.Init(planetUni)
 		homeSector.UnoccupiedPlanets = append(homeSector.UnoccupiedPlanets, planetData)
 	}
-	return homeSector, homePlanet
+	return homeSector, homePlanetId
 }
 
-func occupiedPlanets(occupiedPlanets map[string]models.PlanetUser, homeSectorId string, universe models.Universe) []models.StaticPlanetData {
-	var staticPlanets []models.StaticPlanetData
-	for _, planetUser := range occupiedPlanets {
-		planetUni := universe.Systems[planetUser.Position.SystemId()].Sectors[planetUser.Position.SectorId()].Planets[planetUser.Position.PlanetId()]
-		staticPlanet := models.StaticPlanetData{}
-		staticPlanet.Init(planetUser, planetUni, homeSectorId)
+func occupiedPlanets(occupiedPlanets map[string]repoModels.PlanetUser, homeSectorId string, homeSectorData map[string]repoModels.PlanetUni) []controllerModels.StaticPlanetData {
+	var staticPlanets []controllerModels.StaticPlanetData
+	for planetId := range occupiedPlanets {
+		planetUni := homeSectorData[planetId]
+		staticPlanet := controllerModels.StaticPlanetData{}
+		staticPlanet.Init(planetUni, homeSectorId)
 		staticPlanets = append(staticPlanets, staticPlanet)
 	}
 	return staticPlanets
 }
 
-func findHomePlanet(occupiedPlanets map[string]models.PlanetUser) *models.PlanetPosition {
-	for _, planet := range occupiedPlanets {
+func getHomeSectorData(userData *repoModels.UserData, universeRepository repoModels.UniverseRepository) (*models.PlanetPosition, map[string]repoModels.PlanetUni, error) {
+	var homePlanetPosition models.PlanetPosition
+	for planetId, planet := range userData.OccupiedPlanets {
 		if planet.Home {
-			return &planet.Position
+			homePlanetPosition = models.InitPlanetPositionById(planetId)
+			break
 		}
 	}
-	return nil
+	homeSectorData, err := universeRepository.GetSector(homePlanetPosition.System, homePlanetPosition.Sector)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &homePlanetPosition, homeSectorData, nil
+}
+
+func getClanData(clanId string, clanRepository repoModels.ClanRepository) (*repoModels.ClanData, error) {
+	var clanData *repoModels.ClanData
+	var err error
+	if clanId != "" {
+		clanData, err = clanRepository.FindById(clanId)
+		if err != nil {
+			return nil, err
+		}
+		return clanData, nil
+	}
+	return nil, nil
 }
