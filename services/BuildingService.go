fc5cfd6d115cb7ef32727452c6d056f6ecfe8d79
Devashish <devashish.gupta@target.com>
2021-09-19 05:51:52 +0530

    Adding support for building service

--- services/BuildingService.go
+++ services/BuildingService.go
@@ -1,36 +1,79 @@
 package services
 
 import (
-	"github.com/themane/MMOServer/dao"
+	"errors"
+	"github.com/themane/MMOServer/constants"
+	repoModels "github.com/themane/MMOServer/mongoRepository/models"
+	"strconv"
 	"strings"
 )
 
 const (
-	MINE = "MINE"
+	WATER_MINE    = "WATER_MINE"
+	GRAPHENE_MINE = "GRAPHENE_MINE"
 )
 
-func UpgradeBuilding(username string, planetId string, buildingId string) (string, string) {
-	waterConstants := dao.GetWaterConstants()
-	grapheneConstants := dao.GetGrapheneConstants()
-	userData := dao.GetUserData(username)
-	buildingType := getBuildingType(buildingId)
-	for key, planetUser := range userData.OccupiedPlanets {
-		if planetUser.Position.PlanetId() == planetId {
-			switch buildingType {
-			case MINE:
-				msg, err := upgradeMiningPlant(&planetUser, buildingId, waterConstants, grapheneConstants)
-				userData.OccupiedPlanets[key] = planetUser
-				dao.UpdateUserData(username, userData)
-				return msg, err
-			}
-		}
+func UpgradeBuilding(username string, planetId string, buildingId string, userRepository repoModels.UserRepository) error {
+	waterConstants := constants.GetWaterConstants()
+	grapheneConstants := constants.GetGrapheneConstants()
+
+	userData, err := userRepository.FindByUsername(username)
+	if err != nil {
+		return err
 	}
-	return "", "Invalid username or planet_id or building_id"
+	waterRequired, grapheneRequired, shelioRequired, err := verifyAndGetRequiredResources(*userData, planetId, buildingId, getBuildingType(buildingId), waterConstants, grapheneConstants)
+	if err != nil {
+		return err
+	}
+	err = userRepository.UpgradeBuildingLevel(userData.Id, planetId, buildingId, waterRequired, grapheneRequired, shelioRequired)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func verifyAndGetRequiredResources(userData repoModels.UserData, planetId string, buildingId string, buildingType string,
+	waterConstants constants.ResourceConstants, grapheneConstants constants.ResourceConstants) (int, int, int, error) {
+
+	buildingLevel := userData.OccupiedPlanets[planetId].Buildings[buildingId].BuildingLevel
+	nextBuildingLevelString := strconv.Itoa(buildingLevel + 1)
+	switch getBuildingType(buildingId) {
+	case WATER_MINE:
+		if waterConstants.MaxLevel <= buildingLevel {
+			return 0, 0, 0, errors.New("max level reached")
+		}
+		waterRequired := waterConstants.Levels[nextBuildingLevelString].WaterRequired
+		grapheneRequired := waterConstants.Levels[nextBuildingLevelString].GrapheneRequired
+		shelioRequired := waterConstants.Levels[nextBuildingLevelString].ShelioRequired
+		if userData.OccupiedPlanets[planetId].Water.Amount >= waterRequired &&
+			userData.OccupiedPlanets[planetId].Graphene.Amount >= grapheneRequired &&
+			userData.OccupiedPlanets[planetId].Shelio >= shelioRequired {
+			return 0, 0, 0, errors.New("not enough resources")
+		}
+		return waterRequired, grapheneRequired, shelioRequired, nil
+	case GRAPHENE_MINE:
+		if grapheneConstants.MaxLevel <= buildingLevel {
+			return 0, 0, 0, errors.New("max level reached")
+		}
+		waterRequired := grapheneConstants.Levels[nextBuildingLevelString].WaterRequired
+		grapheneRequired := grapheneConstants.Levels[nextBuildingLevelString].GrapheneRequired
+		shelioRequired := grapheneConstants.Levels[nextBuildingLevelString].ShelioRequired
+		if userData.OccupiedPlanets[planetId].Water.Amount >= waterRequired &&
+			userData.OccupiedPlanets[planetId].Graphene.Amount >= grapheneRequired &&
+			userData.OccupiedPlanets[planetId].Shelio >= shelioRequired {
+			return 0, 0, 0, errors.New("not enough resources")
+		}
+		return waterRequired, grapheneRequired, shelioRequired, nil
+	}
+	return 0, 0, 0, errors.New("building not found")
 }
 
 func getBuildingType(buildingId string) string {
-	if strings.HasPrefix(buildingId, "WMP") || strings.HasPrefix(buildingId, "GMP") {
-		return MINE
+	if strings.HasPrefix(buildingId, "WMP") {
+		return WATER_MINE
+	}
+	if strings.HasPrefix(buildingId, "GMP") {
+		return GRAPHENE_MINE
 	}
 	return ""
 }
