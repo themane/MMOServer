477b1217d5031f4de57d0a31fc7bd8d205377e9e
Devashish <devashish.gupta@target.com>
2021-09-19 05:56:38 +0530

    Adding support for building controller

--- controllers/BuildingController.go
+++ controllers/BuildingController.go
@@ -3,13 +3,24 @@ package controllers
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/themane/MMOServer/models"
+	controllerModels "github.com/themane/MMOServer/controllers/models"
+	repoModels "github.com/themane/MMOServer/mongoRepository/models"
 	"github.com/themane/MMOServer/services"
 	"io/ioutil"
 	"log"
 )
 
-// UpgradeBuildingController godoc
+type BuildingController struct {
+	userRepository repoModels.UserRepository
+}
+
+func NewBuildingController(userRepository *repoModels.UserRepository) *BuildingController {
+	return &BuildingController{
+		userRepository: *userRepository,
+	}
+}
+
+// UpgradeBuilding godoc
 // @Summary Upgrade Building API
 // @Description Upgrade API for new building or upgrading built one
 // @Tags Upgrade
@@ -20,9 +31,9 @@ import (
 // @Param building_id query string true "building identifier"
 // @Success 200 {object} models.LoginResponse
 // @Router /login [post]
-func UpgradeBuildingController(c *gin.Context) {
+func (b *BuildingController) UpgradeBuilding(c *gin.Context) {
 	body, _ := ioutil.ReadAll(c.Request.Body)
-	var request models.UpgradeBuildingRequest
+	var request controllerModels.UpgradeBuildingRequest
 	err := json.Unmarshal(body, &request)
 	if err != nil {
 		log.Print(err)
@@ -31,10 +42,10 @@ func UpgradeBuildingController(c *gin.Context) {
 	}
 	log.Printf("Upgrading: %s, %s, %s", request.Username, request.PlanetId, request.BuildingId)
 
-	response, err2 := services.UpgradeBuilding(request.Username, request.PlanetId, request.BuildingId)
-	if len(err2) > 0 {
-		c.JSON(500, models.UpdateResponse{Message: response, Error: err2})
+	err = services.UpgradeBuilding(request.Username, request.PlanetId, request.BuildingId, b.userRepository)
+	if err != nil {
+		c.JSON(500, controllerModels.UpdateResponse{Error: err.Error()})
 		return
 	}
-	c.JSON(200, models.UpdateResponse{Message: response, Error: err2})
+	c.JSON(200, controllerModels.UpdateResponse{Message: "Successfully upgraded"})
 }
