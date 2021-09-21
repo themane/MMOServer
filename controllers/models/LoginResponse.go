16fc88e5313b680428b74a3f48218d685f0974d7
Devashish <devashish.gupta@target.com>
2021-09-21 18:21:01 +0530

    Removing HomePlanetId and using same_sector instead of home to avoid confusion

--- controllers/models/LoginResponse.go
+++ controllers/models/LoginResponse.go
@@ -8,18 +8,17 @@ import (
 type LoginResponse struct {
 	Profile         Profile            `json:"profile"`
 	HomeSector      Sector             `json:"home_sector"`
-	HomePlanetId    string             `json:"home_planet_id"`
 	OccupiedPlanets []StaticPlanetData `json:"occupied_planets"`
 }
 
 type StaticPlanetData struct {
 	PlanetConfig string                `json:"planet_config" example:"Planet2.json"`
 	Position     models.PlanetPosition `json:"position"`
-	Home         bool                  `json:"home" example:"true"`
+	SameSector   bool                  `json:"same_sector" example:"true"`
 }
 
 func (p *StaticPlanetData) Init(planetUni repoModels.PlanetUni, homeSectorId string) {
 	p.PlanetConfig = planetUni.PlanetConfig
-	p.Home = homeSectorId == planetUni.Position.SectorId()
+	p.SameSector = homeSectorId == planetUni.Position.SectorId()
 	p.Position = planetUni.Position.Clone()
 }
