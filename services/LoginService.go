38d3397a3453ff63ba82574a77968581be4ffca5
Devashish <devashish.gupta@target.com>
2021-10-10 23:35:29 +0530

    Adding support for mission data retrieval in service

--- services/LoginService.go
+++ services/LoginService.go
@@ -6,12 +6,14 @@ import (
 	controllerModels "github.com/themane/MMOServer/controllers/models"
 	"github.com/themane/MMOServer/models"
 	repoModels "github.com/themane/MMOServer/mongoRepository/models"
+	"log"
 )
 
 type LoginService struct {
 	userRepository          repoModels.UserRepository
 	clanRepository          repoModels.ClanRepository
 	universeRepository      repoModels.UniverseRepository
+	missionRepository       repoModels.MissionRepository
 	userExperienceConstants constants.ExperienceConstants
 	clanExperienceConstants constants.ExperienceConstants
 	buildingConstants       map[string]constants.BuildingConstants
@@ -25,6 +27,7 @@ func NewLoginService(
 	userRepository repoModels.UserRepository,
 	clanRepository repoModels.ClanRepository,
 	universeRepository repoModels.UniverseRepository,
+	missionRepository repoModels.MissionRepository,
 	experienceConstants map[string]constants.ExperienceConstants,
 	buildingConstants map[string]constants.BuildingConstants,
 	mineConstants map[string]constants.MiningConstants,
@@ -35,6 +38,7 @@ func NewLoginService(
 		userRepository:          userRepository,
 		clanRepository:          clanRepository,
 		universeRepository:      universeRepository,
+		missionRepository:       missionRepository,
 		buildingConstants:       buildingConstants,
 		userExperienceConstants: experienceConstants[constants.UserExperiences],
 		clanExperienceConstants: experienceConstants[constants.ClanExperiences],
@@ -61,7 +65,11 @@ func (l *LoginService) Login(username string) (*controllerModels.LoginResponse, 
 
 	var response controllerModels.LoginResponse
 	response.Profile.Init(*userData, clanData, l.userExperienceConstants)
-	response.HomeSector = l.home(userData.OccupiedPlanets, *homePlanetPosition, homeSectorData)
+	homeSector, err := l.home(userData.OccupiedPlanets, *homePlanetPosition, homeSectorData)
+	if err != nil {
+		return nil, err
+	}
+	response.HomeSector = *homeSector
 	response.OccupiedPlanets, err = l.occupiedPlanets(userData.OccupiedPlanets, homePlanetPosition.SectorId(), homeSectorData)
 	if err != nil {
 		return nil, err
@@ -70,7 +78,7 @@ func (l *LoginService) Login(username string) (*controllerModels.LoginResponse, 
 }
 
 func (l *LoginService) home(allOccupiedPlanetIds map[string]repoModels.PlanetUser,
-	homePlanetPosition models.PlanetPosition, homeSectorData map[string]repoModels.PlanetUni) controllerModels.Sector {
+	homePlanetPosition models.PlanetPosition, homeSectorData map[string]repoModels.PlanetUni) (*controllerModels.Sector, error) {
 
 	var homeSector controllerModels.Sector
 	homeSector.Position.Init(homePlanetPosition)
@@ -78,7 +86,18 @@ func (l *LoginService) home(allOccupiedPlanetIds map[string]repoModels.PlanetUse
 	for planetId, planetUni := range homeSectorData {
 		if planetUser, ok := allOccupiedPlanetIds[planetId]; ok {
 			planetData := controllerModels.OccupiedPlanet{}
+			attackMissions, err := l.missionRepository.FindAttackMissionsFromPlanetId(planetId)
+			if err != nil {
+				log.Println("error in retrieving attack missions for: "+planetId, err)
+				return nil, errors.New("error in retrieving attack missions for: " + planetId)
+			}
+			spyMissions, err := l.missionRepository.FindSpyMissionsFromPlanetId(planetId)
+			if err != nil {
+				log.Println("error in retrieving spy missions for: "+planetId, err)
+				return nil, errors.New("error in retrieving spy missions for: " + planetId)
+			}
 			planetData.Init(planetUni, planetUser,
+				attackMissions, spyMissions,
 				l.buildingConstants,
 				l.waterConstants, l.grapheneConstants,
 				l.defenceConstants, l.shipConstants,
@@ -95,7 +114,7 @@ func (l *LoginService) home(allOccupiedPlanetIds map[string]repoModels.PlanetUse
 		}
 		homeSector.UnoccupiedPlanets = append(homeSector.UnoccupiedPlanets, planetData)
 	}
-	return homeSector
+	return &homeSector, nil
 }
 
 func (l *LoginService) occupiedPlanets(occupiedPlanets map[string]repoModels.PlanetUser,
