8e4553d1ecd853bdb22e7a13e3a5e81cd57003bf
Devashish <devashish.gupta@target.com>
2021-10-27 23:47:06 +0530

    Refactoring models

--- mongoRepository/models/NotificationData.go
+++ mongoRepository/models/NotificationData.go
@@ -1,16 +1,16 @@
 package models
 
-import controllerModels "github.com/themane/MMOServer/controllers/models"
+import "github.com/themane/MMOServer/models"
 
 type Notification struct {
-	Id           string                        `json:"_id" bson:"_id"`
-	Notification controllerModels.Notification `json:"notification" bson:"notification"`
-	Sent         bool                          `json:"sent" bson:"sent"`
-	UserId       string                        `json:"user_id" bson:"user_id"`
+	Id           string              `json:"_id" bson:"_id"`
+	Notification models.Notification `json:"notification" bson:"notification"`
+	Sent         bool                `json:"sent" bson:"sent"`
+	UserId       string              `json:"user_id" bson:"user_id"`
 }
 
 type NotificationRepository interface {
 	FindByUserId(userId string) ([]Notification, error)
-	AddNotification(notification controllerModels.Notification, userId string) (*Notification, error)
+	AddNotification(notification models.Notification, userId string) (*Notification, error)
 	MarkSent(Id string) error
 }
