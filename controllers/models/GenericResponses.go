298fa56dbd297cc3bd93f4be9de8cea4ee519aa6
Devashish <devashish.gupta@target.com>
2021-10-27 23:42:41 +0530

    Correcting repository models

--- controllers/models/GenericResponses.go
+++ controllers/models/GenericResponses.go
@@ -2,36 +2,37 @@ package models
 
 import (
 	"github.com/themane/MMOServer/constants"
+	"github.com/themane/MMOServer/models"
 	repoModels "github.com/themane/MMOServer/mongoRepository/models"
 )
 
 type UserResponse struct {
-	Profile         Profile            `json:"profile"`
-	HomeSector      Sector             `json:"home_sector"`
-	OccupiedPlanets []StaticPlanetData `json:"occupied_planets"`
-	Notifications   []Notification     `json:"notifications"`
+	Profile         Profile               `json:"profile"`
+	HomeSector      Sector                `json:"home_sector"`
+	OccupiedPlanets []StaticPlanetData    `json:"occupied_planets"`
+	Notifications   []models.Notification `json:"notifications"`
 }
 
 type PlanetResponse struct {
-	OccupiedPlanet OccupiedPlanet `json:"occupied_planet"`
-	Notifications  []Notification `json:"notifications"`
+	OccupiedPlanet OccupiedPlanet        `json:"occupied_planet"`
+	Notifications  []models.Notification `json:"notifications"`
 }
 
 type UserPlanetResponse struct {
-	Base                    bool                 `json:"base" example:"true"`
-	Resources               Resources            `json:"resources"`
-	Population              Population           `json:"population"`
-	Shields                 []Shield             `json:"shields"`
-	IdleDefences            []Defence            `json:"idle_defences" bson:"idle_defences"`
-	IdleDefenceShipCarriers []DefenceShipCarrier `json:"defence_ship_carriers" bson:"defence_ship_carriers"`
-	Home                    bool                 `json:"home" example:"true"`
-	Notifications           []Notification       `json:"notifications"`
+	Base                    bool                  `json:"base" example:"true"`
+	Resources               Resources             `json:"resources"`
+	Population              Population            `json:"population"`
+	Shields                 []Shield              `json:"shields"`
+	IdleDefences            []Defence             `json:"idle_defences" bson:"idle_defences"`
+	IdleDefenceShipCarriers []DefenceShipCarrier  `json:"defence_ship_carriers" bson:"defence_ship_carriers"`
+	Home                    bool                  `json:"home" example:"true"`
+	Notifications           []models.Notification `json:"notifications"`
 }
 
 func (p *UserPlanetResponse) Init(planetUser repoModels.PlanetUser,
 	buildingConstants map[string]constants.BuildingConstants,
 	defenceConstants map[string]constants.DefenceConstants, shipConstants map[string]constants.ShipConstants,
-	notifications []Notification) {
+	notifications []models.Notification) {
 
 	if planetUser.Base {
 		p.Base = true
