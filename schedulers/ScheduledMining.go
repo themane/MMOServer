489967e259b8db6ae3e53861634a35c538cb5201
Devashish <devashish.gupta@target.com>
2021-10-27 21:01:21 +0530

    Moving mining utils to constants

--- schedulers/ScheduledMining.go
+++ schedulers/ScheduledMining.go
@@ -11,7 +11,7 @@ func (j *ScheduledJobManager) scheduledMining() {
 	for system := 0; system < j.maxSystem; system++ {
 		occupiedPlanets, err := j.universeRepository.GetAllOccupiedPlanets(system)
 		if err != nil {
-			j.logger.Error("error in retrieving all occupied planets for system: "+string(system), err)
+			j.logger.Error("error in retrieving all occupied planets for system: "+strconv.Itoa(system), err)
 			return
 		}
 		userIdplanetsMap := map[string][]models.PlanetUni{}
@@ -25,61 +25,22 @@ func (j *ScheduledJobManager) scheduledMining() {
 			}
 		}
 		for userId, planets := range userIdplanetsMap {
-			planetIdWaterMiningRateMap, planetIdGrapheneMiningRateMap := j.getMiningRate(userId, planets)
-			err := j.userRepository.ScheduledWaterIncrease(userId, planetIdWaterMiningRateMap)
-			if err != nil {
+			userData, err1 := j.userRepository.FindById(userId)
+			if err1 != nil {
+				j.logger.Error("error in retrieving user data for: "+userId, err)
+				return
+			}
+			planetIdWaterMiningRateMap, planetIdGrapheneMiningRateMap := constants.GetMiningRate(*userData, planets, j.waterConstants, j.grapheneConstants)
+			err1 = j.userRepository.ScheduledWaterIncrease(userId, planetIdWaterMiningRateMap)
+			if err1 != nil {
 				j.logger.Error("error in water increase update for user: "+userId, err)
 				return
 			}
-			err = j.userRepository.ScheduledGrapheneIncrease(userId, planetIdGrapheneMiningRateMap)
-			if err != nil {
+			err1 = j.userRepository.ScheduledGrapheneIncrease(userId, planetIdGrapheneMiningRateMap)
+			if err1 != nil {
 				j.logger.Error("error in graphene increase update for user: "+userId, err)
 				return
 			}
 		}
 	}
-}
-
-func (j *ScheduledJobManager) getMiningRate(userId string, occupiedPlanets []models.PlanetUni) (map[string]map[string]int, map[string]map[string]int) {
-	userData, err := j.userRepository.FindById(userId)
-	if err != nil {
-		j.logger.Error("error in retrieving user data for: "+userId, err)
-		return nil, nil
-	}
-	planetIdWaterMiningRateMap := map[string]map[string]int{}
-	planetIdGrapheneMiningRateMap := map[string]map[string]int{}
-	for _, planetUni := range occupiedPlanets {
-		planetUser := userData.OccupiedPlanets[planetUni.Id]
-		for _, mineUni := range planetUni.Mines {
-			mineUser := planetUser.Mines[mineUni.Id]
-			miningPlant := planetUser.Buildings[mineUser.MiningPlantId]
-
-			var miningRatePerWorker int
-			if mineUni.Type == constants.Water {
-				miningRatePerWorker = j.waterConstants.Levels[strconv.Itoa(miningPlant.BuildingLevel)].MiningRatePerWorker
-				miningRate := j.getTotalMiningRate(miningRatePerWorker, miningPlant.Workers, mineUni.MaxLimit, mineUser.Mined)
-				if planetIdWaterMiningRateMap[planetUni.Id] == nil {
-					planetIdWaterMiningRateMap[planetUni.Id] = map[string]int{}
-				}
-				planetIdWaterMiningRateMap[planetUni.Id][mineUni.Id] = miningRate
-			}
-			if mineUni.Type == constants.Graphene {
-				miningRatePerWorker = j.grapheneConstants.Levels[strconv.Itoa(miningPlant.BuildingLevel)].MiningRatePerWorker
-				miningRate := j.getTotalMiningRate(miningRatePerWorker, miningPlant.Workers, mineUni.MaxLimit, mineUser.Mined)
-				if planetIdGrapheneMiningRateMap[planetUni.Id] == nil {
-					planetIdGrapheneMiningRateMap[planetUni.Id] = map[string]int{}
-				}
-				planetIdGrapheneMiningRateMap[planetUni.Id][mineUni.Id] = miningRate
-			}
-		}
-	}
-	return planetIdWaterMiningRateMap, planetIdGrapheneMiningRateMap
-}
-
-func (j *ScheduledJobManager) getTotalMiningRate(miningRatePerWorker int, miningPlantWorkers int, maxMinedLimit int, minedResource int) int {
-	miningRate := miningRatePerWorker * miningPlantWorkers
-	if maxMinedLimit < (minedResource + miningRate) {
-		miningRate = maxMinedLimit - (minedResource + miningRate)
-	}
-	return miningRate
 }
