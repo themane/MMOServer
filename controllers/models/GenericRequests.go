bcfac0617737132ca707847ae4d79a3800fa84c2
Devashish <devashish.gupta@target.com>
2022-02-27 22:22:24 +0530

    Commenting for refactoring

--- controllers/models/GenericRequests.go
+++ controllers/models/GenericRequests.go
@@ -4,26 +4,26 @@ type LoginRequest struct {
 	Username string `json:"username" example:"devashish"`
 }
 
-type RefreshRequest struct {
-	Username string `json:"username" example:"devashish"`
-	PlanetId string `json:"planet_id" example:"001:002:03"`
-}
-
-type RefreshMineRequest struct {
-	Username string `json:"username" example:"devashish"`
-	PlanetId string `json:"planet_id" example:"001:002:03"`
-	MineId   string `json:"mine_id" example:"G018"`
-}
-
-type UpgradeBuildingRequest struct {
-	Username   string `json:"username" example:"devashish"`
-	PlanetId   string `json:"planet_id" example:"001:002:03"`
-	BuildingId string `json:"building_id" example:"GMP0018"`
-}
-
-type UpdateBuildingWorkersRequest struct {
-	Username   string `json:"username" example:"devashish"`
-	PlanetId   string `json:"planet_id" example:"001:002:03"`
-	BuildingId string `json:"building_id" example:"GMP0018"`
-	Workers    int    `json:"workers" example:"10"`
-}
+//type RefreshRequest struct {
+//	Username string `json:"username" example:"devashish"`
+//	PlanetId string `json:"planet_id" example:"001:002:03"`
+//}
+//
+//type RefreshMineRequest struct {
+//	Username string `json:"username" example:"devashish"`
+//	PlanetId string `json:"planet_id" example:"001:002:03"`
+//	MineId   string `json:"mine_id" example:"G018"`
+//}
+//
+//type UpgradeBuildingRequest struct {
+//	Username   string `json:"username" example:"devashish"`
+//	PlanetId   string `json:"planet_id" example:"001:002:03"`
+//	BuildingId string `json:"building_id" example:"GMP0018"`
+//}
+//
+//type UpdateBuildingWorkersRequest struct {
+//	Username   string `json:"username" example:"devashish"`
+//	PlanetId   string `json:"planet_id" example:"001:002:03"`
+//	BuildingId string `json:"building_id" example:"GMP0018"`
+//	Workers    int    `json:"workers" example:"10"`
+//}
