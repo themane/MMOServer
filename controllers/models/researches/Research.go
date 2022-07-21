485e0bfa7410a0bd6de6f757502a332dc20d4559
tHemaNe <devagpta@gmail.com>
2022-07-21 22:07:35 +0530

    Correcting Research Bonus format

--- controllers/models/researches/Research.go
+++ controllers/models/researches/Research.go
@@ -1,6 +1,7 @@
 package researches
 
 import (
+	"fmt"
 	"github.com/themane/MMOServer/constants"
 	repoModels "github.com/themane/MMOServer/mongoRepository/models"
 	"strconv"
@@ -10,10 +11,15 @@ type Research struct {
 	Name                  string                 `json:"name" example:"POPULATION_CONTROL_CENTER"`
 	Description           string                 `json:"description"`
 	Level                 int                    `json:"level" example:"3"`
-	Bonus                 map[string]interface{} `json:"bonus"`
-	NextLevelBonus        map[string]interface{} `json:"next_level_bonus"`
+	Bonus                 []ResearchBonus        `json:"bonus"`
+	NextLevelBonus        []ResearchBonus        `json:"next_level_bonus"`
 	ResearchState         State                  `json:"research_state"`
 	NextLevelRequirements *NextLevelRequirements `json:"next_level_requirements"`
+}
+
+type ResearchBonus struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
 
 func InitAllResearches(planetUser repoModels.PlanetUser,
@@ -34,18 +40,21 @@ func InitAllResearches(planetUser repoModels.PlanetUser,
 			Level:       currentLevel,
 		}
 		if currentLevel > 0 {
-			r.Bonus = researchConstant.Bonus[strconv.Itoa(currentLevel)]
+			for k, v := range researchConstant.Bonus[strconv.Itoa(currentLevel)] {
+				value := fmt.Sprintf("%#v", v)
+				r.Bonus = append(r.Bonus, ResearchBonus{k, value})
+			}
 		}
 		if currentLevel < researchConstant.MaxLevel {
-			r.NextLevelBonus = researchConstant.Bonus[strconv.Itoa(currentLevel+1)]
+			for k, v := range researchConstant.Bonus[strconv.Itoa(currentLevel+1)] {
+				value := fmt.Sprintf("%#v", v)
+				r.NextLevelBonus = append(r.NextLevelBonus, ResearchBonus{k, value})
+				if currentLevel == 0 {
+					r.Bonus = append(r.Bonus, ResearchBonus{k, "0"})
+				}
+			}
 			r.NextLevelRequirements = &NextLevelRequirements{}
 			r.NextLevelRequirements.Init(currentLevel, researchConstant)
-		}
-		if currentLevel == 0 {
-			r.Bonus = map[string]interface{}{}
-			for k := range r.NextLevelBonus {
-				r.Bonus[k] = 0
-			}
 		}
 		r.ResearchState.Init(research, researchConstant)
 		researches = append(researches, r)
